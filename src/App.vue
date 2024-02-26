35e449fb3aafcc52d236ff10f9a6edf5a01d090a
DanielVasc <estudante.danielvasc@gmail.com>
2024-02-26 14:40:24 -0300

    novo

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,35 @@
 <script setup>
-import { ref } from 'vue'
+  import { ref } from 'vue';
 
-const contador = ref(0)
+  const contador = ref(0);
 
-function decrementar(){
-  contador.value--
-}
+  //função para adicionar um numero
+  function incrementar() {
+    contador.value++; 
+    // .value é usado para se referir ao valor, Já o ++ usamos para nos referir a aumentar em 1
+  }
+
+   //função para tirar um numero
+  function decrementar() {
+    if (contador.value > 0) {
+      contador.value--;
+      // mesma logica que o incrementar
+    }
+  }
+
+   //função para reiniciar, voltar ao 0
+  function reiniciar() {
+    contador.value = 0;
+    //esse codigo faz com que o valor do contador volte para o 0
+  }
 </script>
 
 <template>
-  <button @click="contador++">+</button>
-  <button @click="decrementar">-</button>
-  <p>Contador: {{ contador }}</p>
+  <div>
+    <h1>Contador</h1>
+    <p> {{ contador }} </p>
+    <button @click="incrementar">Incrementar</button>
+    <button @click="decrementar">Decrementar</button>
+    <button @click="reiniciar">Reiniciar</button>
+  </div>
 </template>
-
-<style scoped>
-
-</style>
