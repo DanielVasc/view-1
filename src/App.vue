72e1766b48d48c74f7156d03ad9cddb88877ed18
DanielVasc <estudante.danielvasc@gmail.com>
2024-03-08 16:34:12 -0300

    feito

--- src/App.vue
+++ src/App.vue
@@ -1,68 +1,15 @@
-<!-- aula 1 -->
-
-<!-- <script setup>
-  import { ref } from 'vue';
-
-  const contador = ref(0);
-
-  //função para adicionar um numero
-  function incrementar() {
-    contador.value++; 
-    // .value é usado para se referir ao valor, Já o ++ usamos para nos referir a aumentar em 1
-  }
-
-   //função para tirar um numero
-  function decrementar() {
-    if (contador.value > 0) {
-      contador.value--;
-      // mesma logica que o incrementar
-    }
-  }
-
-   //função para reiniciar, voltar ao 0
-  function reiniciar() {
-    contador.value = 0;
-    //esse codigo faz com que o valor do contador volte para o 0
-  }
-</script>
-
-<template>
-  <div>
-    <h1>Contador</h1>
-    <p> {{ contador }} </p>
-    <button @click="incrementar">Incrementar</button>
-    <button @click="decrementar">Decrementar</button>
-    <button @click="reiniciar">Reiniciar</button>
-  </div>
-</template> -->
-
-
-
-
-
-<!-- aula 2 -->
-
 <script setup>
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 
 const contador = ref(0);
-const nome = 'Daniel'
-const idade = 16
-
-function inverter(texto) {
-  return texto.split('').reverse().join('').toUpperCase()
-}
-
-function saudacao() {
-  return `Olá, ${nome}!`
-}
+const contador2 = ref(0);
 
 function incrementar() {
   contador.value++;
 }
 
-function incrementar10() {
-  contador.value+= 10;
+function incrementar2() {
+  contador2.value++;
 }
 
 function decrementar() {
@@ -71,9 +18,9 @@ function decrementar() {
   }
 }
 
-function decrementar10() {
+function decrementar2() {
   if (contador.value > 0) {
-    contador.value-=10;
+    contador.value--;
   }
 }
 
@@ -81,82 +28,106 @@ function reiniciar() {
   contador.value = 0;
 }
 
+function reiniciar2() {
+  contador2.value = 0;
+}
+
+let juntar = computed(() => {
+  return contador.value + contador2.value
+});
+
+let parImpar = computed(() => {
+  return juntar.value % 2 === 0 ? 'par' : 'ímpar'
+});
+
+const soma = computed(() => {
+return contador.value + contador2.value
+});
 </script>
 
+
 <template>
-  <div class="bloco">
-  <div class="info">
-    <h1>Exemplo 1 - Mostrando informações na tela</h1>
-    <p>Nome: {{ nome }} </p>
-    <p>Idade: {{ idade }} anos</p>
-    <hr />
-    <p>Para completar 50 anos faltam: {{ 50 - idade }} anos</p>
-    <p>O nome tem {{ nome.length }} caracteres</p>
-    <p>O nome invertido é: {{ inverter(nome) }} </p>
-    <hr />
-    <p> Exemplo de saudação usando função</p>
-    <p> {{ saudacao() }} </p>
+  <div class="contador">
+    <div class="info">
+      <h1>Contador</h1>
+      <p :color="red"> {{ contador }} </p>
+      <button @click="incrementar">+1</button>
+      <button @click="decrementar">-1</button>
+      <button @click="reiniciar">Reiniciar</button>
+      <div v-if="contador > 10"> o número é maior que 10</div>
+      <div v-else>O número é menor que 10 </div>
+    </div>
   </div>
 
   <div class="contador">
-  <div v-if="contador < 10" class="vermelho">
+    <div class="info">
       <h1>Contador</h1>
-      <p :color="red"> {{ contador }} </p>
-      <button @click="incrementar10">+10</button>
-      <button @click="incrementar">+1</button>
-      <button @click="decrementar">-1</button>
-      <button @click="decrementar10">-10</button>
-      <button @click="reiniciar">Reiniciar</button>
-    </div>
-
-    <div v-else="contador > 10" class="verde">
-      <h1>Contador</h1>
-      <p :color="red"> {{ contador }} </p>
-      <button @click="incrementar10">+10</button>
-      <button @click="incrementar">+1</button>
-      <button @click="decrementar">-1</button>
-      <button @click="decrementar10">-10</button>
-      <button @click="reiniciar">Reiniciar</button>
+      <p :color="red"> {{ contador2 }} </p>
+      <button @click="incrementar2">+1</button>
+      <button @click="decrementar2">-1</button>
+      <button @click="reiniciar2">Reiniciar</button>
+      <div v-if="contador2 > 10"> o número é maior que 10</div>
+      <div v-else>O número é menor que 10 </div>
     </div>
   </div>
+
+  <div class="Sominha">
+    <h1>Soma</h1>
+    <p :color="red">{{ soma }}</p>
+    <div v-if="contador + contador2 > 10">A soma é maior que 10</div>
+    <div v-else>A soma é menor que 10 </div>
+  </div>
+
+  <div class="parouimpar">
+    <h1>Par ou ímpar</h1>
+    <p :color="red">{{ parImpar }}</p>
+  </div>
+
+  <div class="booleano">
+    <h1>Verdadeiro ou falso</h1>
+  <p :color="red">{{ contador + contador2 > 30 ? 'Sim' : 'Não' }}</p>
 </div>
 </template>
 
 <style scoped>
-.info {
-  background-color: rgba(35, 12, 22, 1);
-  padding: 20px 30px;
-  color: rgb(183, 210, 219);
-  border-radius: 10px;
-}
+  .info {
+    background-color: rgba(35, 12, 22, 1);
+    padding: 40px 50px;
+    color: rgb(183, 210, 219);
+    border-radius: 10px;
+  }
 
-.contador {
-  padding: 20px 30px;
-  color: rgb(183, 210, 219);
-  border-radius: 10px;
-}
+  .contador {
+    padding: 20px 30px;
+    color: rgb(183, 210, 219);
+    border-radius: 10px;
+    margin-top: 20px;
+  }
 
-.contador p {
-  color: aliceblue;
-}
+  h1 {
+    color: white;
+    font-weight: bold;
+    font-size: 3em;
+  }
 
-h1 {
-  color: white;
-  font-weight: bold;
-  font-size: 1.5em;
-}
+  .Sominha {
+    padding: 20px 30px;
+    color: rgb(97, 121, 129);
+    border-radius: 10px;
+    margin-top: 20px;
+  }
 
-.vermelho {
-  background-color: rgb(153, 9, 9);
-  padding: 20px 30px;
-  color: rgb(183, 210, 219);
-  border-radius: 10px;
-}
+  .parouimpar {
+    padding: 20px 30px;
+    color: rgb(97, 121, 129);
+    border-radius: 10px;
+    margin-top: 20px;
+  }
 
-.verde {
-  background-color: rgb(50, 143, 7);
-  padding: 20px 30px;
-  color: rgb(183, 210, 219);
-  border-radius: 10px;
-}
+  .booleano {
+    padding: 20px 30px;
+    color: rgb(97, 121, 129);
+    border-radius: 10px;
+    margin-top: 20px;
+  }
 </style>
